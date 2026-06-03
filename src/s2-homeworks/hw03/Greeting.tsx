45bb500c52d40abc043ec1eb68a2b64de731a72d
Kirill1387 <bykris1337@gmail.com>
2026-06-03 13:29:20 +0300

    new work

--- src/s2-homeworks/hw03/Greeting.tsx
+++ src/s2-homeworks/hw03/Greeting.tsx
@@ -57,13 +57,13 @@ const Greeting: React.FC<GreetingPropsType> = (
                     className={s.button}
                     disabled={!name.trim()}
                 >
-                    add
+                    Add
                 </button>
 
             </div>
             {lastUserName && (
                 <div className={s.greeting}>
-                    Привет <span id={'hw3-last-user'}>{lastUserName}</span>!
+                    Привет <span id={'hw3-last-user'}>{lastUserName}</span>
                 </div>
             )}
         </div>
